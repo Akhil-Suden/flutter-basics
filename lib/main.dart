91d192859b0a3e354458fdedc2007e3e01c068a0
Akhil <akhilsuden@gmail.com>
2023-12-16 13:42:45 +0530

    my first flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('hello, ninjas!'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-      onPressed: PrintHi,
-    ),
-  ),
+  home: Home()
 ));
 
 void PrintHi(){
   print('Hi');
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Center(
+        child: Text(
+            'hello, ninja!',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower',
+            )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: PrintHi,
+        backgroundColor: Colors.lightBlue,
+      ),
+    );
+  }
 }
